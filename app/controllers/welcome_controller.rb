0db21bfb913ee6c442607e12480e02393f1ff590
AndresFTR <afelipe0113@gmail.com>
2019-03-08 17:55:57 -0500

    added some styles

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,8 @@
 class WelcomeController < ApplicationController
   def index
-
+    @clients = Client.all
+    @affairs = Affair.all
+    @attorneys = Attorney.all
+    @assignments = Assignment.all
   end
 end
